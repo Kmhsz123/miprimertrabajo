3a524ff793f180c66accd2ac51d446b639d8d4c9
Kmhsz123 <146156538+Kmhsz123@users.noreply.github.com>
2023-09-27 18:47:21 -0600

    Add files via upload

---
+++ fibonucci original 5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_original
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             static void Fibonacci()
+            {
+                Console.Write("Ingrese el número de términos de la sucesión de Fibonacci a mostrar: ");
+                int n = int.Parse(Console.ReadLine());
+
+                int a = 0, b = 1;
+
+                Console.WriteLine("Sucesión de Fibonacci:");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(a);
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+            }
+
+        }
+    }
+}
---
+++ pares e impares real.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// trabajo de  numeros pares e impares 
+
+{
+    Console.WriteLine(" escriba un numero ");
+    int numero = Convert.ToInt64(Console.ReadLine());
+    int sobrante = numero % 2;
+    if (sobrante == 0) ;
+    {
+
+        Console.WriteLine(" el numero es par ");
+    }
+    else
+
+    {
+
+
+        Console.WriteLine(" el numero es impar ");
+    }
+
+
+
+
+
+}
+
